8ade587dc4ff6d3dc920bef3576b24c150690e31
jmgorduez <jmgorduez@gmail.com>
2019-05-22 10:39:15 -0500

    Method parseEmployeePaySheet() implemented

--- src/main/java/ec/com/jmgorduez/ACMEEmployeesPayment/domain/EmployeePaySheetParser.java
+++ src/main/java/ec/com/jmgorduez/ACMEEmployeesPayment/domain/EmployeePaySheetParser.java
@@ -3,12 +3,28 @@ package ec.com.jmgorduez.ACMEEmployeesPayment.domain;
 import ec.com.jmgorduez.ACMEEmployeesPayment.domain.abstractions.IEmployeePaySheet;
 import ec.com.jmgorduez.ACMEEmployeesPayment.domain.abstractions.IEmployeePaySheetParser;
 import ec.com.jmgorduez.ACMEEmployeesPayment.domain.abstractions.IPayable;
+import ec.com.jmgorduez.ACMEEmployeesPayment.utils.Constants;
 
 import java.util.function.Function;
+import java.util.stream.Stream;
+
+import static ec.com.jmgorduez.ACMEEmployeesPayment.utils.Constants.*;
 
 public class EmployeePaySheetParser implements IEmployeePaySheetParser {
     @Override
     public IEmployeePaySheet parseEmployeePaySheet(String line, Function<String, IPayable> getPayableItem) {
-        return null;
+        IEmployeePaySheet result = new EmployeePaySheet(getName(line));
+        getWorkingTimes(line)
+                .map(getPayableItem)
+                .forEach(result::addPayableItem);
+        return result;
+    }
+
+    private String getName(String line){
+        return line.split(ASSIGNMENT_CHARACTER)[ZERO];
+    }
+
+    private Stream<String> getWorkingTimes(String line){
+        return Stream.of(line.substring(line.indexOf(ASSIGNMENT_CHARACTER)).split(COMMA_CHARACTER));
     }
 }
--- src/main/java/ec/com/jmgorduez/ACMEEmployeesPayment/utils/Constants.java
+++ src/main/java/ec/com/jmgorduez/ACMEEmployeesPayment/utils/Constants.java
@@ -2,9 +2,14 @@ package ec.com.jmgorduez.ACMEEmployeesPayment.utils;
 
 public class Constants {
 
+    public static final Integer ZERO = 0;
+
     public static final Float _15 = 15F;
     public static final Float _20 = 20F;
     public static final Float _25 = 25F;
     public static final Float _30 = 30F;
     public static final Float _60 = 60F;
+
+    public static final String ASSIGNMENT_CHARACTER = "=";
+    public static final String COMMA_CHARACTER = ",";
 }
