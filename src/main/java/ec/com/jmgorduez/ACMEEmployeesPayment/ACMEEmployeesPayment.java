8ca73d1276b35518a408a1124e4b073de8fdc827
jmgorduez <jmgorduez@gmail.com>
2019-05-24 10:01:47 -0500

    Code refactoring

--- src/main/java/ec/com/jmgorduez/ACMEEmployeesPayment/ACMEEmployeesPayment.java
+++ src/main/java/ec/com/jmgorduez/ACMEEmployeesPayment/ACMEEmployeesPayment.java
@@ -8,7 +8,6 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
