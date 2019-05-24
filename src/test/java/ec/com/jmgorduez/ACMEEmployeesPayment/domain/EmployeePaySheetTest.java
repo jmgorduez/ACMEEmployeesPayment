72510cf1f65f99c03fb0c803e4c9d4223cfe6354
jmgorduez <jmgorduez@gmail.com>
2019-05-24 14:40:15 -0500

    Test addWorkingTimeVerifyIfSetBasicUnitOfTimeIsCalled() implemented

--- src/test/java/ec/com/jmgorduez/ACMEEmployeesPayment/domain/EmployeePaySheetTest.java
+++ src/test/java/ec/com/jmgorduez/ACMEEmployeesPayment/domain/EmployeePaySheetTest.java
@@ -1,12 +1,14 @@
 package ec.com.jmgorduez.ACMEEmployeesPayment.domain;
 
 import ec.com.jmgorduez.ACMEEmployeesPayment.dataGenerator.TestDataGenerator;
+import ec.com.jmgorduez.ACMEEmployeesPayment.domain.abstractions.IWorkingTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static ec.com.jmgorduez.ACMEEmployeesPayment.dataGenerator.TestDataGenerator.*;
 import static ec.com.jmgorduez.ACMEEmployeesPayment.utils.Constants._09_00;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 class EmployeePaySheetTest {
 
@@ -34,12 +36,22 @@ class EmployeePaySheetTest {
     }
 
     @Test
-    void addPayableItem() {
+    void addWorkingTime() {
         assertThat(employeePaySheetUnderTest.workingTimes.isEmpty())
                 .isTrue();
-        employeePaySheetUnderTest.addWorkingTime(new WorkingTime(_09_00, _11_30, TestDataGenerator::numberOfHours,
-                TestDataGenerator::_15_USD_Per_Hours));
+        employeePaySheetUnderTest.addWorkingTime(mockWorkingTime());
         assertThat(employeePaySheetUnderTest.workingTimes.size())
                 .isEqualTo(ONE);
+    }
+
+    @Test
+    void addWorkingTimeVerifyIfSetBasicUnitOfTimeIsCalled() {
+        IWorkingTime workingTime = mockWorkingTime();
+        employeePaySheetUnderTest.addWorkingTime(workingTime);
+        verify(workingTime, times(ONE)).setBasicUnitOfTime(TestDataGenerator::numberOfHours);
+    }
+
+    private IWorkingTime mockWorkingTime(){
+        return mock(IWorkingTime.class);
     }
 }
